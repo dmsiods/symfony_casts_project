01f64f9421c2770a74d734321f2e15ddc414d68c
sidorkoda <dmsiods0@gmail.com>
2022-08-26 10:25:11 +0300

    twig changes

--- app/src/Controller/QuestionController.php
+++ app/src/Controller/QuestionController.php
@@ -2,21 +2,32 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     #[Route('/question', 'question')]
     public function question(Request $request): Response
     {
-        return new Response('Hello from question and QuestionController');
+        echo 'Hello from question and QuestionController';
+
+        return $this->render('base.html.twig', []);
     }
 
     #[Route('/question/{slug}', 'show_question')]
     public function show_question($slug): Response
     {
-        return new Response('Hello from question and QuestionController ' . $slug);
+        $answer = 'Hello from question and QuestionController';
+//        return new Response(
+//            sprintf($answer . ' ' . '%s', ucwords(str_replace('_', '-', $slug), '-'))
+//        );
+        return $this->render('question/show.html.twig', [
+            'slug' => ucwords(str_replace('_', '-', $slug), '-'),
+            'random_array' => ['qwerty', 'Dima', 'blablabla'],
+            'answer' => $answer,
+        ]);
     }
 }
