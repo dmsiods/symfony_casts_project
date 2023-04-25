270579f80fc0a88a44ea3cc2a80cb05fd67e335b
sidorkoda <dmsiods0@gmail.com>
2023-04-25 14:58:04 +0300

    symfony 6 ep 14

--- app/src/Controller/SongController.php
+++ app/src/Controller/SongController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SongController extends AbstractController
 {
-    #[Route('/api/songs/{id<\d+>}', methods: ['GET'], name: 'api_songs_get_one')]
+    #[Route('/api/songs/{id<\d+>}', name: 'api_songs_get_one', methods: ['GET'])]
     public function getSong(int $id, LoggerInterface $logger): Response
     {
         // TODO query the database
@@ -19,9 +19,9 @@ class SongController extends AbstractController
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
 
-//        $logger->info('Returning API response for song {song}', [
-//            'song' => $id,
-//        ]);
+        $logger->info('Returning API response for song {song}', [
+            'song' => $id,
+        ]);
 
         return $this->json($song);
     }
