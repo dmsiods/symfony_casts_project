2f1522b0aa8156059323ecad25130552386e7d77
sidorkoda <dmsiods0@gmail.com>
2023-04-27 09:51:55 +0300

    Symfony 6. Fundamentals. ep. 2

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
 ];
