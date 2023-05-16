24b3ea087169b3b9e28c5fd4f6f00893f794e4f8
dima <dmsiods0@gmail.com>
2023-05-16 11:56:42 +0300

    Symfony 6. Fundamentals. ep. 20

--- app/src/Service/MixRepository.php
+++ app/src/Service/MixRepository.php
@@ -1,33 +1,32 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Service;
 
 use Psr\Cache\CacheItemInterface;
 use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MixRepository
 {
     public function __construct(
-        private HttpClientInterface $httpClient,
+        private HttpClientInterface $githubContentClient,
         private CacheInterface $cache,
+        #[Autowire('%kernel.debug%')]
         private bool $isDebug
-    ) {
-    }
+    ) {}
 
     public function findAll(): array
     {
-        dump($this->isDebug);
+        /*
+        $output = new BufferedOutput();
+        $this->twigDebugCommand->run(new ArrayInput([]), $output);
+        dd($output);
+        */
 
-        return $this->cache->get('mixes_data', function (CacheItemInterface $cacheItem) {
+        return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem) {
             $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
-
-            $response = $this->httpClient->request(
-                'GET',
-                'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json'
-            );
+            $response = $this->githubContentClient->request('GET', '/SymfonyCasts/vinyl-mixes/main/mixes.json');
 
             return $response->toArray();
         });
